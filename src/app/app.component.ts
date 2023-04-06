89001d938a93d298f4dffa1eec5dfd4b7c60e18f
MSD55 <vallabhaimadhavshri99@gmail.com>
2023-04-06 20:09:27 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-forms';
+  title = 'uploadfile';
+
+  
+  displayMultipleImages!: Boolean;
+  displayMultipleImageArray!: Array<any>;
+  
+
+  
+
+  @ViewChild('multipleInput', { static: false })
+  multipleInput!: ElementRef;
+
+  images: any;
+  multipleImages = [];
+
+  constructor(private http: HttpClient) {
+    //this.displaySingleImage = false;
+    this.displayMultipleImageArray = [];
+    this.displayMultipleImages = false;
+    //this.displaySingleImageArray = [];
+  }
+
+  
+
+  
+
+  selectMultipleImage(event: any) {
+    if (event.target.files.length > 0) {
+      this.multipleImages = event.target.files;
+    }
+  }
+
+  onMultipleSubmit() {
+    const formdata = new FormData()
+
+    for (let img of this.multipleImages) {
+      formdata.append('files',img)
+    }
+
+    this.http.post<any>('http://localhost:3000/multipleFiles', formdata)
+      .subscribe((res) => {
+        console.log(res)
+        this.multipleInput.nativeElement.value = ""
+        console.log(res.path)
+        this.displayMultipleImages=true
+        this.displayMultipleImageArray = res.path
+        
+    })
+  }
 }
